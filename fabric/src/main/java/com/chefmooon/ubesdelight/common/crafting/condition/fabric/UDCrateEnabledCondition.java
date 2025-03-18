5cb39ae5561079ddb8f8d97fc8502e4b0586d334
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-03-18 17:28:50 -0400

    update crate crafting condition

--- fabric/src/main/java/com/chefmooon/ubesdelight/common/crafting/condition/fabric/UDCrateEnabledCondition.java
+++ fabric/src/main/java/com/chefmooon/ubesdelight/common/crafting/condition/fabric/UDCrateEnabledCondition.java
@@ -3,17 +3,17 @@ package com.chefmooon.ubesdelight.common.crafting.condition.fabric;
 import com.chefmooon.ubesdelight.common.Configuration;
 import com.chefmooon.ubesdelight.common.utility.TextUtils;
 import com.mojang.serialization.MapCodec;
-import io.github.fabricators_of_create.porting_lib.conditions.ICondition;
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceCondition;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditionType;
 import net.minecraft.core.HolderLookup;
 import org.jetbrains.annotations.Nullable;
 
-public class UDCrateEnabledCondition implements ICondition {
+public class UDCrateEnabledCondition implements ResourceCondition {
     public static final MapCodec<UDCrateEnabledCondition> CODEC = MapCodec.unit(new UDCrateEnabledCondition());
     public static final ResourceConditionType<UDCrateEnabledCondition> TYPE = ResourceConditionType.create(TextUtils.res("ud_crates_enabled"), CODEC);
 
     @Override
-    public boolean test(HolderLookup.@Nullable Provider registryLookup, IContext context) {
+    public boolean test(HolderLookup.@Nullable Provider registryLookup) {
         return Configuration.isEnableUDCropCrates();
     }
 
