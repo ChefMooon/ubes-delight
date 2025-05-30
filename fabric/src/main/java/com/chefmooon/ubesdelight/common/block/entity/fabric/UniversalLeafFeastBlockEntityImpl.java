2c9407738f7adbbb0b4320e5893686394192e113
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-05-30 13:52:54 -0400

    code cleanup

--- fabric/src/main/java/com/chefmooon/ubesdelight/common/block/entity/fabric/UniversalLeafFeastBlockEntityImpl.java
+++ fabric/src/main/java/com/chefmooon/ubesdelight/common/block/entity/fabric/UniversalLeafFeastBlockEntityImpl.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.Nullable;
 import vectorwing.farmersdelight.common.block.entity.SyncedBlockEntity;
 import vectorwing.farmersdelight.refabricated.inventory.ItemStackHandler;
 
-// TODO: portinglib found here
 public class UniversalLeafFeastBlockEntityImpl extends SyncedBlockEntity {
     public static final int MAX_ITEMS = UniversalLeafFeastBlockEntity.MAX_ITEMS;
     private final ItemStackHandler inventory;
