9e74040b0791fd332b93c0d06aee27a851878449
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-11-27 09:52:48 -0500

    improve: eiv add processing stages to results

--- fabric/src/main/java/com/chefmooon/ubesdelight/integration/eiv/baking_mat/fabric/BakingMatViewRecipe.java
+++ fabric/src/main/java/com/chefmooon/ubesdelight/integration/eiv/baking_mat/fabric/BakingMatViewRecipe.java
@@ -94,6 +94,7 @@ public class BakingMatViewRecipe implements IEivViewRecipe {
     public List<SlotContent> getResults() {
         List<SlotContent> results = new ArrayList<>(this.mandatoryOutputs);
         results.addAll(this.chanceOutputs.keySet());
+        results.addAll(this.processStages);
         return results;
     }
 }
