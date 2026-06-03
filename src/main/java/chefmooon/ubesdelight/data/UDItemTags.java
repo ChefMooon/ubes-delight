bf8cc6c13333c37188befbfdc059e87d91366548
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2026-06-03 16:40:50 -0400

    fix: remove tag cabbage_roll_ingredients

--- src/main/java/chefmooon/ubesdelight/data/UDItemTags.java
+++ src/main/java/chefmooon/ubesdelight/data/UDItemTags.java
@@ -97,10 +97,6 @@ public class UDItemTags extends ItemTagsProvider {
                 .add(UbesDelightItems.ARROZ_CALDO.get())
                 .add(UbesDelightItems.MECHADO.get());
 
-        // Farmers Delight Item Tags
-        tag(CompatibilityTags.FARMERS_DELIGHT_CABBAGE_ROLL_INGREDIENTS)
-                .add(UbesDelightItems.UBE.get());
-
         tag(CompatibilityTags.FARMERS_DELIGHT_WILD_CROPS_ITEM)
                 .add(UbesDelightItems.WILD_UBE.get())
                 .add(UbesDelightItems.WILD_GARLIC.get())
